0741ef7932470b949c2f5478eacb5987ca5eae8f
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-05-21 11:16:23 +0530

    form set up for firebase backend

---
+++ merchendis_ecommerce/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+import SignUpForm from "./form";
+
+function App() {
+  return (
+    <div className="App">
+<SignUpForm /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ merchendis_ecommerce/src/form.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import './SignUpForm.css';
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validateForm(formData);
+    if (Object.keys(validationErrors).length === 0) {
+      // Form is valid, proceed with submission
+      console.log('Form submitted:', formData);
+      setFormData({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+      setErrors({});
+    } else {
+      setErrors(validationErrors);
+    }
+  };
+
+  const validateForm = (data) => {
+    let errors = {};
+
+    if (!data.username.trim()) {
+      errors.username = 'Username is required';
+    }
+
+    if (!data.email.trim()) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+      errors.email = 'Email is invalid';
+    }
+
+    if (!data.password) {
+      errors.password = 'Password is required';
+    } else if (data.password.length < 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    }
+
+    if (!data.confirmPassword) {
+      errors.confirmPassword = 'Confirm Password is required';
+    } else if (data.confirmPassword !== data.password) {
+      errors.confirmPassword = 'Passwords do not match';
+    }
+
+    return errors;
+  };
+
+  return (
+    <div class="container">
+  <div class="signup-form-container">
+    <div className="signup-form">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          {errors.username && <span className="error">{errors.username}</span>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <span className="error">{errors.password}</span>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+          {errors.confirmPassword && (
+            <span className="error">{errors.confirmPassword}</span>
+          )}
+        </div>
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+    </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
