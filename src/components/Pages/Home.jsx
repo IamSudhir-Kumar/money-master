26727a8e143c6f991cb3b0697afb01c45960669e
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-12-09 22:45:17 +0530

    layout problem

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -5,7 +5,15 @@ import image2 from './images/logo.svg'
 const Home = () => {
     return (
         <>
-           <img src={image2} alt="home" className="absolute object-cover w-1/3 rounded-md" /> 
+            <div className="container m-auto grid grid-cols-3 gap-2 md:grid-cols-5 lg:grid-cols-8">
+                <div className="tile col-span-3 bg-teal-500 md:col-span-5 lg:col-span-8">
+                    <h1 className="tile-marker">ONe</h1>
+                </div>
+                <div class="tile col-span-1 row-start-2 row-end-5 bg-amber-500 md:col-span-2 lg:col-span-3">
+                    <h1 class="tile-marker">TWO</h1>
+                </div>
+                {/* <img src={image2} alt="home" className="absolute object-cover w-1/3 rounded-md" />  */}
+            </div>
         </>
     )
 }
