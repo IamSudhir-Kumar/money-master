85f5e431fbf88cecd296e73e82d6a92d9404211b
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-12-17 00:21:48 +0530

    EASY text layout set

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -5,16 +5,21 @@ const Home = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-1">
       {/* Row 1 */}
-      <div className="bg-white">
-        <img src={logo} alt="" className='w-80 h-80 float-left rounded-lg ps-5'/>
-        <div className=""
-        >Mobilizing Ambition, Nurturing Dreams, Empowering Success
-        </div>
-    </div>
-      <div className="bg-[#1cb95c] p-8">Item 2</div>
+      <div className="bg-white flex items-center">
+        <img src={logo} alt="" className='w-70 h-80 rounded-lg'/>
+        <div className="text-center text-2xl font-normal ps-4">
+          <span className="font-bold text-green-500 font-pacifico hover:text-green-800">E</span> mpowering <br/>
+          <span className="font-bold text-green-500 font-pacifico  hover:text-green-800">A</span> spirations <br/>
+          <span className="font-bold text-green-500 font-pacifico  hover:text-green-800">S</span> haping <br/>
+          <span className="font-bold text-green-500 font-pacifico  hover:text-green-800">Y</span> outh<br/>
+        </div>   
+      </div>
 
       {/* Row 2 */}
+      <div className="bg-[#1cb95c] p-8">Item 2</div>
       <div className="bg-[#1cb95c] p-8">Item 3</div>
+
+      {/* Row 3 */}
       <div className="bg-[#1cb95c] p-8">Item 4</div>
     </div>
   );
